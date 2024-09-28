f8b9e689abb83700b2638cb23a57ca84c29fab11
Rishabh <rgmaheshwari06@gmail.com>
2024-09-28 13:27:43 +0530

    Update package-lock.json and package.json
    - Add "backend_project" as a dependency in package-lock.json and package.json.
    - Update the MongoDB connection URI in src/db/index.js.
    - Refactor user.controller.js to improve code readability and add comments.
    - Update multer.middle.js to fix a typo in the filename function.
    - Update user.model.js to fix indentation and add comments.

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose"
 import { DB_NAME } from "../constants.js"
-
+const MONGODB_URI = `mongodb://localhost:27017/`;
 const connectDB=async()=>{
     try{
-        const connectionInstance=await mongoose.connect
-        (`${process.env.MONGODB_URI}/
-            ${DB_NAME}
-
-        }`)
+        const connectionInstance=await mongoose.connect(
+        `${MONGODB_URI}/${DB_NAME}}`
+    )
         console.log(`\n MongoDB connected succesfully !!
-             DB HOST: ${connectionInstance.connection.host}`);
+            DB HOST: ${connectionInstance.connection.host}`);
         
 
     }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,99 +1,92 @@
+    import mongoose,{Schema} from "mongoose";
+    import jwt from "jsonwebtoken"
+    import bcrypt from "bcrypt"
 
+    const UserSchema=new Schema(
+        {
+            username:{
+                type:String,
+                required:true,
+                unique:true,
+                lowercase:true,
+                trim:true,
+                index:true
 
+            },
+            email:{
+                type:String,
+                required:true,
+                unique:true,
+                lowercase:true,
+                trim:true,
+                
 
-import mongoose,{Schema} from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
-
-const UserSchema=new Schema(
-    {
-        username:{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true,
-            index:true
-
-        },
-        email:{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true,
+            },
+            fullName:{
+                type:String,
+                required:true,
+                
+                lowercase:true,
+                trim:true,
+            },
+            avatar:{
+                type:String,//cloudinary url            required:true,
+            required:true
+            },
+            coverImage:{
+                type:String,//cloudinary url            required:true,
             
-
-        },
-        fullName:{
-            type:String,
-            required:true,
-            
-            lowercase:true,
-            trim:true,
-        },
-        avatar:{
-            type:String,//cloudinary url            required:true,
-           required:true
-        },
-        coverImage:{
-            type:String,//cloudinary url            required:true,
-           
-        },
-        watchhistory:[
-            {
-                type:Schema.Types.ObjectId,
-                ref:"Video"
+            },
+            watchhistory:[
+                {
+                    type:Schema.Types.ObjectId,
+                    ref:"Video"
+                }
+            ],
+            password:{
+                type:String,
+                required:[true,"password is required"]
+            },
+            refreshtoken:{
+                type:String
             }
-        ],
-        password:{
-            type:String,
-            required:[true,"password is required"]
+    },
+    {timestamps:true})
+    UserSchema.pre("save",async function(next)
+    {
+
+    if(!this.isModified("password")) return next();
+        this.password=await bcrypt.hash(this.password,10)
+    next()
+    }
+    )
+
+    UserSchema.methods.isPasswordCorrect= async function (password){
+    return await bcrypt.compare(password,this.password)
+    }
+
+    UserSchema.methods.generateAcessToken=function(){
+        return jwt.sign({
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullName
         },
-        refreshtoken:{
-            type:String
-        }
+    process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+    }
 
-
-
-
-},
-{timestamps:true})
-UserSchema.pre("save",async function(next)
-{
-
-if(!this.isModified("password")) return next();
-    this.password=await bcrypt.hash(this.password,10)
-next()
-}
-)
-
-UserSchema.methods.isPasswordCorrect= async function (password){
-   return await bcrypt.compare(password,this.password)
-}
-
-UserSchema.methods.generateAcessToken=function(){
-    return jwt.sign({
-        _id:this._id,
-        email:this.email,
-        username:this.username,
-        fullname:this.fullName
-    },
-process.env.ACCESS_TOKEN_SECRET,{
-    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-})
-}
-
-UserSchema.methods.generateRefreshToken=function(
-    
-){
-    return jwt.sign({
-        _id:this._id, 
+    UserSchema.methods.generateRefreshToken=function(
         
-    },
-process.env.REFRESH_TOKEN_SECRET,{
-    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-})
-}
+    ){
+        return jwt.sign({
+            _id:this._id, 
+            
+        },
+    process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+    }
 
-export const User=mongoose.model("User",UserSchema)
+    export const User=mongoose.model("User",UserSchema)
